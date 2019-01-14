0d48abc476c11e4773edf4331a448f7bc410f97d
cnorick <cnathanorick@gmail.com>
2019-01-13 20:01:54 -0800

    Update calendar event

--- js/scripts.js
+++ js/scripts.js
@@ -188,23 +188,23 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Ram and Antara's Wedding",
+            title: "Nathan and Sarah's Wedding",
 
             // Event start date
-            start: new Date('Nov 27, 2017 10:00'),
+            start: new Date('2019-06-15T18:00:00.000-04:00'),
 
             // Event duration (IN MINUTES)
-            // duration: 120,
+            duration: 4 * 60,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
+            // end: new Date('Nov 29, 2017 00:00'),
 
             // Event Address
-            address: 'ITC Fortune Park Hotel, Kolkata',
+            address: '805 Hardin Ln, Sevierville, TN 37862',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues contact Mr. Amit Roy at +91 9435021804 or +91 7086018971."
+            description: "We can't wait to see you on our big day. For any queries or issues contact Mr. Nathan Orick at +18658515520."
         }
     });
 
