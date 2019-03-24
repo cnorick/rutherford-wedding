f839c1ba10d4774b672e292446e1ce67f705de3d
cnorick <cnathanorick@gmail.com>
2019-03-23 21:51:00 -0700

    Add invite code

--- js/scripts.js
+++ js/scripts.js
@@ -293,8 +293,7 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We\'re saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
+        if (MD5($('#invite_code').val()) !== '6e746846ad849391755c57db7166827c') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
             $.post('https://script.google.com/macros/s/AKfycbyPrWqSgI5GtAdPmv7_s5BqgFKZs4grEv0WiVrh-yUlEC4Z8fk/exec', data)
